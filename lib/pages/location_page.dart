682c4a1769d9a2d3099cd631b0a5b822011a25db
Tamuzo77 <tamuzomenga@gmail.com>
2023-12-27 16:49:27 +0100

    Creation des pages de l'ecran principal

---
+++ lib/pages/location_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LocationPage extends StatelessWidget {
+  const LocationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Location Page")
+        ],
+      ),
+    );;
+  }
+}
