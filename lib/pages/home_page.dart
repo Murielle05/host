4ebeec43d5b5e03ea6d56d19b4330e967d32d9cc
Tamuzo77 <tamuzomenga@gmail.com>
2023-12-27 17:58:00 +0100

    searchField

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:host/components/searchField.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(10),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("HomePage")
+          Text(
+              "Find deals ",
+            style: GoogleFonts.daysOne(
+              fontSize: 30,
+              fontWeight: FontWeight.w400
+            ),
+          ),
+          Text(
+              "on the best Hotels ",
+            style: GoogleFonts.daysOne(
+              fontSize: 30,
+              fontWeight: FontWeight.w400
+            ),
+          ),
+
+          SizedBox(height: 15,),
+
+          Row(
+            children: [
+              SearchField(),
+              Icon(Icons.sync_alt_sharp),
+            ],
+          )
         ],
       ),
     );
