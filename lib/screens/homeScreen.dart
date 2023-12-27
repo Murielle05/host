8a64d64ff8712c03c8313229cb021acbc7b4d339
Murielle05 <murielle.bokossa05@gmail.com>
2023-12-27 18:45:25 +0100

    Mise en place du Drawer

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -7,6 +7,33 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+                child: Text("Hostel Five Stars")),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+            ),
+            SizedBox(height: 10,),
+            ListTile(
+              leading: Icon(Icons.location_on_outlined),
+              title: Text("Location"),
+            ),
+            SizedBox(height: 10,),
+            ListTile(
+              leading: Icon(Icons.calendar_today_outlined),
+              title: Text("Calendar"),
+            ),
+            SizedBox(height: 10,),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text(""),
         actions: [
