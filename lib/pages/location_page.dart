e04ccf9ed87ae52202591e3aaf7d19b00d013150
Tamuzo77 <tamuzomenga@gmail.com>
2023-12-28 19:47:31 +0100

    Location page terminé

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -1,17 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class LocationPage extends StatelessWidget {
+class LocationPage extends StatefulWidget {
   const LocationPage({super.key});
 
   @override
+  State<LocationPage> createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  Location location = new Location();
+
+  bool? _serviceEnabled;
+  PermissionStatus? _permissionGranted;
+  LocationData? _locationData;
+
+  void initializedLocation () async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled!) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled!) {
+        return;
+      }
+    }
+
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+  _permissionGranted = await location.requestPermission();
+  if (_permissionGranted != PermissionStatus.granted) {
+  return;
+  }
+  }
+
+  _locationData = await location.getLocation();
+  print("longitude: ${_locationData!.longitude}, lat: ${_locationData!.latitude} " );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initializedLocation();
+  }
+  static LatLng cotonou = LatLng(6.3724685, 2.3261369);
+  static LatLng paris = LatLng(48.866667, 2.333333);
+
+  Marker _cotonouMarker = Marker(
+    markerId: MarkerId("cotonou"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+    position: cotonou,
+    infoWindow: InfoWindow(
+        title: "Cotonou"
+    ),
+  );
+
+  Marker _parisMarker = Marker(
+      markerId: MarkerId("paris"),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      position: paris,
+      infoWindow: InfoWindow(
+      title: "Paris",
+    )
+  );
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Location Page")
-        ],
-      ),
-    );;
+    return GoogleMap(
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+        markers: {
+          _cotonouMarker,
+          _parisMarker
+        },
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+            target: cotonou,
+            zoom: 17
+        ),
+      );
   }
 }
