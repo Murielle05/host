cc97e9bd857d9cb73c2273064e23d54eb9403693
Tamuzo77 <tamuzomenga@gmail.com>
2023-12-28 20:01:02 +0100

    Calendar finished

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_clean_calendar/controllers/clean_calendar_controller.dart';
+import 'package:scrollable_clean_calendar/scrollable_clean_calendar.dart';
+import 'package:scrollable_clean_calendar/utils/enums.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Calendar ")
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 48.0),
+      child: ScrollableCleanCalendar(
+        calendarController: CleanCalendarController(minDate: DateTime.now(), maxDate: DateTime(2024, 12)),
+        layout: Layout.BEAUTY,
       ),
-    );;
+    );
   }
 }
